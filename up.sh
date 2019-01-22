1f4d5cdd0a019a0b51ef3f374c306410f459b126
mpooley <mpooley@exponent.com>
2019-01-22 13:46:01 -0500

    Fixing up.sh

--- up.sh
+++ up.sh
@@ -9,20 +9,20 @@
 #	. up.sh n -> moves up n directories
 #	(NB: there is a ". " before the executable filename)
 #	For convinience, consider placing a suitable alias in .bash_profile e.g.: alias up=". up.sh"
+#	If symbolic link up --> up.sh is used then the alias can be: alias up=". up"
 #
 # EXAMPLES:
 #	If $PWD=~/<Directory>/<subDirectory> then
 #		. up.sh -> moves to ~/<Directory>
 #		. up.sh 2 -> moves to ~/
-cd .. # always move up at least one directory, even if no argument is passed
-if [ $1 -gt 1 ] 
-then
-	for ((  i = 1 ;  i < $1;  i++  ))
+if [ -z "$1" ]; then # if argument is empty string or null
+	cd .. # always move up at least one directory, even if no argument is passed
+elif [ "$1" -gt 1 ]; then 
+	if [ $# -gt 1 ]; then # check for too many arguments and alert if appropriate (only the 1st argument is used)
+		echo Warning: Too many arguments - $# arguments detected, those after 1st have been ignored
+	fi
+	for ((  i = 0 ;  i < $1;  i++  )) # move up by the passed number of directory levels
 	do
 		cd ..
 	done
-fi
-if [ $# -gt 1 ] # check for too many arguments and alert if appropriate (only the 1st argument is used)
-then
-	echo Warning: Too many arguments - $# arguments detected, those after 1st have been ignored
 fi
