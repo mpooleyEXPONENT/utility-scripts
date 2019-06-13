b86b62b945a0c950a0e90fe2611e36896ac06009
mpooley <mpooley@exponent.com>
2019-06-13 11:28:44 -0400

    add clipboard functionality to smb2win

--- smb2win.sh
+++ smb2win.sh
@@ -7,15 +7,13 @@
 #		returns <windows path>
 # 	e.g.: 
 #		smb2win.sh smb://path/directory/file.txt returns \\path\directory\file.txt
-
 input=$1
 # remove "smb:" if it is included in the input path
 if [ "${input:0:3}" = "smb" ]; then
 	input="${input:4}"
 fi
-output=${input//\//\\} # replace all backslashes with forwardslashes
+output="${input//\//\\}" # replace all backslashes with forwardslashes
 
-# output to terminal
-echo 
-echo "$output"
-echo 
+# output to terminal and copy to clipboard
+echo -n "$output" | tee >(pbcopy) # -n flag needed to prevent linebreak being added to clipboard
+echo # echo to get newline in terminal
