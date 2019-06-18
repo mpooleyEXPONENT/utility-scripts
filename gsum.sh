cc216d1f31119ef393b204eb5b9582f365c530ec
mpooley <mpooley@exponent.com>
2019-06-18 11:27:24 -0400

    adding -s, -a, -d options for specifying source_directory, algorithm, and destination_directory, respectively

--- gsum.sh
+++ gsum.sh
@@ -2,8 +2,25 @@
 # 
 #
 # gsum.sh generates a list of cryptographic hash values for all files within the current directory.
-# sha-256 sums are currently used. A time stamp is prepended to the output file based on the current system time.
-
-# TODO: add command line arguments for path and algorithm
-find . -type f -exec openssl sha256 "{}" + > $(date "+%y%m%d_%H%M%S_%Z")_checksums.txt
-
+# A time stamp is prepended to the output file based on the current system time.
+# Default behavior is to create hash values using SHA256 for all files within the current directory, saving the output to a timestamped .txt file within the same directory
+#
+# USAGE: gsum.sh [-s source_directory] [-d destination_directory] [-a algorithm]
+commandStr="find . -type f -exec openssl sha256 '{}' + > $(date "+%y%m%d_%H%M%S_%Z")_checksums.txt"
+# modify commandStr with supplied optional parameters
+while getopts "s:a:d:" opt; do
+	case $opt in
+		s ) # specify source directory
+		commandStr=${commandStr/./"$OPTARG"}
+        echo $commandStr	
+			;;
+        a ) # specify algorithm
+        commandStr=${commandStr/ openssl sha256 / openssl "$OPTARG" }
+        echo $commandStr
+            ;;
+        d ) # specify distination directory
+        commandStr=${commandStr/> $(date "+%y%m%d_%H%M%S_%Z")_checksums.txt/> "$OPTARG"/$(date "+%y%m%d_%H%M%S_%Z")_checksums.txt}
+        echo $commandStr
+	esac
+done
+eval $commandStr
